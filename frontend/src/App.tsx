3a9c2e93c719ea408a2191ce9a4ab3b044ff9d80
Willian Takanage <williantakanage@gmail.com>
2021-05-04 21:26:04 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "componets/DataTable";
+import Footer from "componets/Footer";
+import NavBar from "componets/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
